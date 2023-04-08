62bd79a64a25072d060ebf5c1e1e1239f81156f7
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-04-08 20:00:58 -0300

    Add linear_search

---
+++ benchmarks/linear_search/main.c
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "vector.h"
+#include "profiler.h"
+
+#define N 10000
+#define M 200
+#define STEP 2000
+
+int main()
+{
+    srand(time(NULL));
+
+    printf("N menor maior media \n");
+
+    for (int n = 1; n <= N + 1; n += STEP)
+    {
+        Vector *v = vector_construct();
+
+        for (int i = 0; i < n; i++)
+            vector_push_back(v, rand());
+
+        double min = 0;
+        double total = 0;
+        double max = 0;
+
+        for (int i = 0; i < M; i++)
+        {
+            int idx = rand() % n;
+
+            int valor = vector_get(v, idx);
+
+            double inicio = get_timestamp();
+
+            vector_find(v, valor);
+
+            double fim = get_timestamp();
+
+            double duracao = fim - inicio;
+
+            min = mmin(min, duracao);
+            max = mmax(max, duracao);
+            total += duracao;
+        }
+
+        double media = total / M;
+
+        printf("%d %g %g %g\n", n, min, max, media);
+
+        vector_destroy(v);
+    }
+
+    return EXIT_SUCCESS;
+}
